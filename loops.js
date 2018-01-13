5c319212ca1bb3a18226e86b0d71a9f5929cfc07
Tom Xu <tomxu89@gmail.com>
2018-01-13 07:24:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,8 +16,10 @@ function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5
   }
+ 
   do {
     array.pop()
   } while (array.length > 0 && maybeTrue())
+ 
   return array
 }
