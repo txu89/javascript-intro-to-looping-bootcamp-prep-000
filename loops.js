03e650b090bdd2bb9db8bf4cedc228fd534bb61c
Tom Xu <tomxu89@gmail.com>
2018-01-13 07:28:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,10 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    array[i] = `I am ${i} strange loop.`
+    if (i=1) {
+      array[i] = `I am ${i} strange loop.`
+    } else {
+      array[i] = `I am ${i} strange loops.`
+    }
   }
   return array
 }
